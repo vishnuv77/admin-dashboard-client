63f95da0209034c48db6cb0ce29942ba965769c7
VishnuV <vishnu@devship.in>
2023-04-18 13:35:08 +0530

    added searc functionality

--- src/components/SubuserTable/SubuserTable.jsx
+++ src/components/SubuserTable/SubuserTable.jsx
@@ -6,9 +6,9 @@ import axios from "axios";
 import "jspdf-autotable";
 import jsPDF from "jspdf";
 
-
 const SubuserTable = ({ onAddUser, onUpdateUser, setId }) => {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     const fetchUsers = async () => {
       const token = localStorage.getItem("token"); // Get the token from local storage
@@ -18,8 +18,8 @@ const SubuserTable = ({ onAddUser, onUpdateUser, setId }) => {
         },
       });
       setUsers(response.data.subUsers);
-      
-     // Set the users state variable to the array of user objects returned by the server
+
+      // Set the users state variable to the array of user objects returned by the server
     };
     console.log(users);
 
@@ -61,7 +61,10 @@ const SubuserTable = ({ onAddUser, onUpdateUser, setId }) => {
 
     // Define the columns and rows for the PDF table
     const columns = ["Username", "Status"];
-    const rows = users.map((user) => [user.email, user.status ? "Active" : "Non-active"]);
+    const rows = users.map((user) => [
+      user.email,
+      user.status ? "Active" : "Non-active",
+    ]);
 
     // Add the table to the PDF document
     doc.autoTable({ head: [columns], body: rows });
@@ -70,6 +73,18 @@ const SubuserTable = ({ onAddUser, onUpdateUser, setId }) => {
     doc.save("subusers.pdf");
   };
 
+
+  //filter functionality implemented to filter it
+  const filteredUsers = users.filter((user) => {
+    const searchText = searchTerm.toLowerCase();
+    return (
+      user.email.toString().toLowerCase().includes(searchText) ||
+      user.status.toString().toLowerCase().includes(searchText)
+    );
+  });
+  
+
+  console.log(searchTerm);
   return (
     <div className="user-table-wrapper" style={{ width: "100%" }}>
       <div className="search-wrapper">
@@ -77,13 +92,17 @@ const SubuserTable = ({ onAddUser, onUpdateUser, setId }) => {
         <input
           type="text"
           className="search-input"
-          placeholder="Search sub users"
+          placeholder="Search sub users here..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
       <button className="add-button" onClick={handleAddUser}>
         Add Sub-user
       </button>
-      <button className="download-button" onClick={handleDownload}> {/* Add the download button */}
+      <button className="download-button" onClick={handleDownload}>
+        {" "}
+        {/* Add the download button */}
         Download Form
       </button>
       <table className="user-table" style={{ width: "100%" }}>
@@ -95,7 +114,7 @@ const SubuserTable = ({ onAddUser, onUpdateUser, setId }) => {
           </tr>
         </thead>
         <tbody>
-          {users?.map((user, index) => {
+          {filteredUsers?.map((user, index) => {
             return (
               <tr key={index}>
                 <td>{user.email}</td>
